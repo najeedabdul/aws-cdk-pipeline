54ef62719e0484084706f153c1e4202b886898a6
najeedabdul <najeed.abdul@gmail.com>
2022-02-07 12:15:48 -0700

    committing lambda

--- lib/aws-cdk-pipeline-stack.ts
+++ lib/aws-cdk-pipeline-stack.ts
@@ -2,19 +2,31 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-//import { MyPipelineAppStage } from './stage';
+import { MyPipelineAppStage } from './stage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    const pipeline = 
+      new CodePipeline(this, 'Pipeline', {
+        pipelineName: 'TestPipeline',
+        synth: new ShellStep('Synth',{
+          input: CodePipelineSource.gitHub('najeedabdul/aws-cdk-pipeline', 'master'),
+          commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        }),
+      });
 
-    new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'TestPipeline',
-      synth: new ShellStep('Synth',{
-        input: CodePipelineSource.gitHub('najeedabdul/aws-cdk-pipeline', 'master'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      }),
-    });
+      const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+        env: {account: "343962227496", region: "us-east-1"}
+      }))
+
+      testingStage.addPost(new ManualApprovalStep('Manual approbal before production'));
+
+      const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+        env: {account: "343962227496", region: "us-east-1"}
+      }))
+
+      }
     }
-  }
+  
---
+++ lib/lambda-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Function, InlineCode, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import { Stack } from 'aws-cdk-lib';
+
+export class MyLambdaStack extends Stack {
+    constructor(scope: Construct, id: string, stageName: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        new Function(this, 'LambdaFunction', {
+            runtime: Runtime.NODEJS_12_X,
+            handler: 'handleer.handler',
+            code: Code.fromAsset(path.join(__dirname, 'lambda')),
+            environment: {"stageName": stageName}
+        });
+    }
+}
